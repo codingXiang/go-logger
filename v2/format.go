61716ce3e64cb7784957d3236a79aae18470ff96
xiang <dryadf66449@gmail.com>
2020-11-10 11:50:41 +0800

    new release: v2

---
+++ v2/format.go
@@ -0,0 +1,32 @@
+package logger
+
+import "github.com/sirupsen/logrus"
+
+type Format int
+
+const (
+	Text Format = iota
+	Json Format
+)
+
+func NewFormat(f string) Format {
+	switch f {
+	case "json":
+		return Json
+	case "text":
+		return Text
+	default:
+		return Text
+	}
+}
+
+func (f Format) Get() logrus.Formatter {
+	switch f {
+	case Json:
+		return &logrus.JSONFormatter{}
+	case Text:
+		return &logrus.TextFormatter{}
+	default:
+		return &logrus.JSONFormatter{}
+	}
+}
